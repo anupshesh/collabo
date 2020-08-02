4a256e270ab7185b6697af92ed9ecbaa59f7aea2
anupshesh <anup.shesh@gmail.com>
2020-08-02 19:18:24 -0400

    correction in canvas

--- src/canvas.js
+++ src/canvas.js
@@ -16,10 +16,6 @@ window.onload = function() // to load an app on opening the page (HTML)
   var touchx = touchy = 0;
   var touchstart = false
   var tooltype = 'draw';
-  var color = 'black';
-  //Establishing server-socket-client connection
-  var socket = io.connect(':3000');
-
 
 //Color listener
           colorInput.addEventListener('input',()=>{
@@ -87,7 +83,6 @@ window.onload = function() // to load an app on opening the page (HTML)
               ctx.lineJoin = ctx.lineCap = 'round';
               ctx.stroke();
 
-
             }
             last_mousex = mousex;
             last_mousey = mousey;
@@ -95,71 +90,10 @@ window.onload = function() // to load an app on opening the page (HTML)
             //Output
             $('#output').html('current: '+mousex+', '+mousey+'<br/>last: '+last_mousex+', '+last_mousey+'<br/>mousedown: '+mousedown);
         });  
-
-        //SENDING DATA TO SERVER
-        $(canvas).on('mousemove', function(e) {
-           if(mousedown) {
-              console.log('Sending: ' + e.clientX + ',' + e.clientY + ',' + tooltype + ',' + mousedown + ',' + ctx.strokeStyle + ',' + mousex + ',' +last_mousex);
-
-              var data = {
-                x: e.clientX,
-                y: e.clientY,
-                useTool: tooltype,
-                useMouse: mousedown,
-                pencolor: ctx.strokeStyle
-                }
-              socket.emit('drawing',data);
-            }
-        });
-
-    //RECEIVING DATA FROM SERVER    
-    socket.on('mouse',newDrawing);
-    function newDrawing(data){
-      ctx.strokeStyle = data.pencolor;
-      var lst_mousex;
-      var lst_mousey;
-      var msx;
-      var msy;
-
-      lst_mousex = msx = parseInt(data.x-canvasx);
-      lst_mousey = msy = parseInt(data.y-canvasy);
-      var use_tool = data.useTool;
-      var msdown = data.useMouse;
-      console.log('Received: ' + data.x + ',' + data.y + ',' + use_tool + ',' + msdown + ',' + ctx.strokeStyle)
-
-            if(msdown) {
-              msx = parseInt(data.x-canvasx);
-              msy = parseInt(data.y-canvasy);
-
-                ctx.beginPath();
-                if(use_tool=='draw') {
-                    ctx.globalCompositeOperation = 'source-over';
-                    ctx.lineWidth = 3;
-                } else {
-                    ctx.globalCompositeOperation = 'destination-out';
-                    ctx.lineWidth = 50;
-                }
-              ctx.moveTo(lst_mousex,lst_mousey);
-              ctx.lineTo(msx,msy);
-              ctx.lineJoin = ctx.lineCap = 'round';
-              ctx.stroke();
-
-            lst_mousex = msx;
-            lst_mousey = msy;
-
-            }
-            
-
-            //Output
-            $('#output').html('current: '+msx+', '+msy+'<br/>last: '+lst_mousex+', '+lst_mousey+'<br/>mousedown: '+msdown);
-        //});  
-
-    }
-
         
   //Use draw|erase
     use_tool = function(tool) {
     tooltype = tool; 
-    }
+    };
   
 }
